f22bf240e2ecca7c6554e6221e80d99b7cc06198
Zhou, Keqin <kezhou@microstrategy.com>
2018-07-01 15:35:29 +0800

    remove client

--- client/src/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const ADD_ROOM = 'ADD_ROOM'
-export const SET_ROLE = 'SET_ROLE'
-export const GET_CONTENT = 'GET_CONTENT'
-export const GET_MASK = 'GET_MASK'
-export const SAVE_RESOURCE =  'SAVE_RESOURCE'
-export const RELOAD_CONTENT = 'RELOAD_CONTENT'
-export const RESET_MASK = 'RESET_MASK'
-
--- client/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux-immutable'
-import {userConfig} from './userConfig'
-
-export default combineReducers({
-  userConfig
-})
